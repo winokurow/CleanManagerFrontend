ef60296155e40dd8f1c647020d7560513d620bff
Ilja Winokurow <ilja_winokurow@yahoo.com>
2022-10-12 22:08:51 +0200

    First commit

---
+++ app/src/main/java/de/zottig/cleanmanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.zottig.cleanmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
